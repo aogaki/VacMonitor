b4c5658a46c51e4853c734d4827ab135ad3ca536
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2020-03-19 14:45:18 +0200

    aaaaa

--- src/TVacMon.cpp
+++ src/TVacMon.cpp
@@ -33,24 +33,12 @@ void TVacMon::InitPort()
 void TVacMon::Write(std::string com)
 {
   fPort->Write(com);
-<<<<<<< HEAD
   fPort->WriteByte(ENQ);
-=======
-  Read();
-  fPort->WriteByte(ENQ);
-  Read();
->>>>>>> 7e8d3a3f440ae62148f210ceaf137e448ec0ffb4
 
   while (fAcqFlag) {
     if (CheckTime()) {
       fPort->Write(com);
-<<<<<<< HEAD
       fPort->WriteByte(ENQ);
-=======
-      Read();
-      fPort->WriteByte(ENQ);
-      Read();
->>>>>>> 7e8d3a3f440ae62148f210ceaf137e448ec0ffb4
     }
 
     usleep(10);
@@ -63,7 +51,6 @@ void TVacMon::Read()
   std::string buf{""};
   const unsigned int timeout_ms = 25;  // timeout value in milliseconds
 
-<<<<<<< HEAD
   while (fAcqFlag) {
     try {
       auto c = fPort->ReadByte(timeout_ms);
@@ -85,22 +72,6 @@ void TVacMon::Read()
 
         // if (fBuffer.size() > 10) DataWrite();
         DataWrite();
-=======
-  try {
-    auto c = fPort->ReadByte(timeout_ms);
-    readFlag = true;
-    buf += c;
-  } catch (const SerialPort::ReadTimeout &timeOut) {
-    if (readFlag) {
-      if (buf != "") {
-        if (buf.find_first_of(',') != std::string::npos)
-          std::cout << buf << std::endl;
-        auto start = buf.find_first_of(',') + 1;  // next of ","
-        auto pressure = std::stod(buf.substr(start, buf.size() - start));
-        auto timeStamp = time(nullptr);
-
-        fBuffer.push_back(MonResult(timeStamp, pressure));
->>>>>>> 7e8d3a3f440ae62148f210ceaf137e448ec0ffb4
       }
       buf.clear();
       readFlag = false;
